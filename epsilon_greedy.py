f2494e76167645d45aeb075d8383064bae2bfd94
Priyanka Miranda <priyankamiranda2@gmail.com>
2020-06-06 00:57:54 +0530

    Epsilon-greedy algo final touches

--- epsilon_greedy.py
+++ epsilon_greedy.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 class my_data:
-  def __init__(self, data_quality):
-    self.data_quality = data_quality # data quality of the dataset
-    self.avg_data_quality = 0 # bandit's mean
-    self.N = 0 # nunmber of time the dataset is fetched 
+	def __init__(self, data_quality):
+		self.data_quality = data_quality # data quality of the dataset
+		self.avg_data_quality = 0 # bandit's mean
+		self.N = 0 # nunmber of time the dataset is fetched 
 
-  def get_data(self):
-    return np.random.randn() + self.data_quality # gaussian with unit variance
+	def get_data(self):
+		print("Data quality : "+str(self.data_quality))	
+		return np.random.randn() + self.data_quality # gaussian with unit variance
 
-  def update_set_params(self, new_data_val):# latest sample received from the exp
-    self.N += 1
-    self.avg_data_quality = (1 - 1.0/self.N)*self.avg_data_quality + 1.0/self.N*new_data_val
+	def update_set_params(self, new_data_val):# latest sample received from the exp
+		self.N += 1
+		self.avg_data_quality = (1 - 1.0/self.N)*self.avg_data_quality + new_data_val/self.N
 
 def select_best_sets(dataset_labels, num_of_good_sets):
 	np.random.shuffle(dataset_labels)
@@ -46,11 +46,11 @@ def exp(total_num_of_sets, num_of_good_sets, num_of_tries, eps, max_data_quality
 				data_quality = min_data_quality + np.random.randint(low=0, high=variance)
 		my_data_qualities.append(data_quality)
 		my_datasets.append(my_data(data_quality)) 
-
+	
 	my_datasets_copies = []
 	for x in range(0,len(eps)):
 		my_datasets_copies.append(my_datasets)
-
+	
 	print("If randomly generated epsilon < eps, We choose randomly among the datasets.")
 	print("Else if randomly generated epsilon > eps, We choose the one with the best average data quality")
 	print("Number of tries we currently have to make sure we use the best data: "+str(num_of_tries))
@@ -60,7 +60,8 @@ def exp(total_num_of_sets, num_of_good_sets, num_of_tries, eps, max_data_quality
 	data = np.zeros(shape=(len(eps),num_of_tries)) # 2d array for n different eps trials and the number of tries
 
 	for i in range(num_of_tries):
-		rand_eps_val = np.random.random()
+		rand_eps_val = np.random.random()*100
+
 		rand_dataset_selected = np.random.choice(len(eps))
 		# print("Epsilon (ε) : "+str(rand_eps_val))
 		for x in range(0,len(eps)):
@@ -70,23 +71,23 @@ def exp(total_num_of_sets, num_of_good_sets, num_of_tries, eps, max_data_quality
 			else:
 				print("highest in current selected")
 				dataset_selected[x] = np.argmax([curr_set.avg_data_quality for curr_set in my_datasets_copies[x]])
-
 			data[x][i] = my_datasets_copies[x][int(dataset_selected[x])].get_data()
-			my_datasets_copies[x][int(dataset_selected[x])].update_set_params(x)
+			my_datasets_copies[x][int(dataset_selected[x])].update_set_params(data[x][i])
 
-	for x in range(0,len(eps)):
-		print()
+
+
+	for x in range(0,len(eps)):		
 		series_avg_accuracy = np.cumsum(data[x]) / (np.arange(num_of_tries) + 1)
 		# plot updated average accuracy as we explore datasets
-		plt.plot(series_avg_accuracy)
+		plt.plot(series_avg_accuracy, label= 'epsilon = '+str(eps[x]))
 
 	for x in range(0,total_num_of_sets):
 		plt.plot(np.ones(num_of_tries)*my_data_qualities[x], 'r--')
 
+	plt.legend()
 	# plt.xscale('log')
 	plt.show()
 
-	return series_avg_accuracy
 
 
 
@@ -103,16 +104,16 @@ def print_data_for_user(total_num_of_sets, num_of_good_sets, num_of_tries, eps, 
 
 if __name__ == '__main__':
 	#---constant params---
-	total_num_of_sets = 3
-	num_of_good_sets = 2
-	max_data_quality = 10
+	total_num_of_sets = 5
+	num_of_good_sets = 1
+	max_data_quality = 100
 	min_data_quality = 1
 	num_of_tries = 1000
-	variance = 3
+	variance = 5
 	#---------------------
 
 	#----varying parms---- 
-	eps = [0.1,0.1,0.1]
+	eps = [90,50,20,10,5]
 	#---------------------
 	print_data_for_user(total_num_of_sets, num_of_good_sets,num_of_tries,eps, max_data_quality, min_data_quality, variance)
-	x = exp(total_num_of_sets, num_of_good_sets,num_of_tries,eps, max_data_quality, min_data_quality, variance)
+	exp(total_num_of_sets, num_of_good_sets,num_of_tries,eps, max_data_quality, min_data_quality, variance)
